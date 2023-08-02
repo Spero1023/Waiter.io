57cff356604d2bc68f6a25cf940e1081671271ba
wchan6 <wchan6.adp@gmail.com>
2023-08-02 17:06:08 -0400

    cleanup

--- client/app/App.js
+++ client/app/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import Navbar from '../features/navbar/Navbar';
+
 import AppRoutes from './AppRoutes';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
       <AppRoutes />
     </div>
   );
