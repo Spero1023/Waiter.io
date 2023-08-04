e064bb675fec904a27651c6f5e88c23e9f970077
Nicholas <n.spera98@gmail.com>
2023-08-04 16:21:34 -0400

    divinci ai for front end code

--- client/features/TranslationDisplay.js
+++ client/features/TranslationDisplay.js
@@ -1,22 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const apiKey = 'ADDYOURKEYHERE!!!!';
+
 function TranslationDisplay({ result, targetLanguage }) {
   const [menu, setMenu] = useState(null);
   const [text, setText] = useState(result);
 
   useEffect(() => {
     setText(result);
-  }, [result]); 
+  }, [result]);
+
+  const prompt = `No extra commentary or pleasantries. Take the following menu and categorize it by food/dish type, include descriptions of allergens, and offer brief descriptions of foreign/non-American cuisine in`;
 
   const handleSubmit = async () => {
     try {
-      const prompt = `No extra commentary or pleasantries. Take the following menu and categorize it by food/dish type, include descriptions of allergens, and offer brief descriptions of foreign/non-American cuisine in ${targetLanguage}: ${text}`;
-      if (result == '' || targetLanguage == null){
-        return alert('No text or target language provided', error);
-      }
-      const response = await axios.post('/reformat-menu', { prompt });
-      setMenu(response.data.reformattedMenu);
+      const response = await axios.post(
+        `https://api.openai.com/v1/engines/text-davinci-003/completions`,
+        {
+          prompt: `${prompt}`,
+          max_tokens: 512,
+          temperature: 0,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+      const generatedText = response.data.choices[0].text;
+      console.log(generatedText);
+
+      setMenu(generatedText);
     } catch (error) {
       console.error('Error calling /api/reformat-menu', error);
     }
@@ -25,10 +41,13 @@ function TranslationDisplay({ result, targetLanguage }) {
   return (
     <div>
       <button onClick={handleSubmit}>Reformat Menu</button>
-      <div>{menu !== null? menu : 'Upload then press Reformat Menu to translate your menu.' }</div>
+      <div>
+        {menu !== null
+          ? menu
+          : 'Upload then press Reformat Menu to translate your menu.'}
+      </div>
     </div>
   );
 }
-
 
 export default TranslationDisplay;
