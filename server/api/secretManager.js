d4e6240b1831c378c5e6f3f57b99bcd394647222
calvinxhe <herb.builder@gmail.com>
2023-08-08 15:20:09 -0400

    secretManager

---
+++ server/api/secretManager.js
@@ -0,0 +1,17 @@
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
+export default async function accessSecret(secretid) {
+    const client = new SecretManagerServiceClient();
+    const secretNew = `projects/waiterio/secrets/${secretid}/versions/latest`
+
+    try {
+        const [version] = await client.accessSecretVersion({
+          name: secretNew,
+        });
+        const secret = version.payload.data.toString();
+        return secret;
+      } catch (error) {
+        console.error(`Failed to access secret: ${error}`);
+        throw error;
+      } 
+}
