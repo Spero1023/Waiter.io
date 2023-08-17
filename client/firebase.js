ea1a171b06890b1826ef0555c69e52765d96df86
calvinxhe <herb.builder@gmail.com>
2023-08-17 12:54:39 -0400

    firebase.js

--- client/firebase.js
+++ client/firebase.js
@@ -1,15 +1,22 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//small comment
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBR7zQ962PV81jdx6RNYqDuv3SzBrDF_Gs',
-  authDomain: 'waiter-io-f84a1.firebaseapp.com',
-  projectId: 'waiter-io-f84a1',
-  storageBucket: 'waiter-io-f84a1.appspot.com',
-  messagingSenderId: '1035985777599',
-  appId: '1:1035985777599:web:8f167a1535f0bb9985270a',
-  measurementId: 'G-Q0SYLZ8KRG',
-}
+  apiKey: 'AIzaSyA1xa0wxt8Soq2AQIRU47yjJFktlvOAgXQ',
+  authDomain: 'waiter-io-395214.firebaseapp.com',
+  projectId: 'waiter-io-395214',
+  storageBucket: 'waiter-io-395214.appspot.com',
+  messagingSenderId: '379346806294',
+  appId: '1:379346806294:web:0fbb4afdf6943fff97f737',
+  measurementId: 'G-2852GR17V5',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = app.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
