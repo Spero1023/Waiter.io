5334f03c59718e32a5b2186250fbabfe46b8835b
Nicholas <n.spera98@gmail.com>
2023-08-17 11:02:30 -0400

    t15

--- client/firebase.js
+++ client/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAZ2nPw4MX7jnmAAjv4j7Qb2YUZGCeutFA",
-    authDomain: "waiter-io-395214.firebaseapp.com",
-    projectId: "waiter-io-395214",
-    storageBucket: "waiter-io-395214.appspot.com",
-    messagingSenderId: "379346806294",
-    appId: "1:379346806294:web:0fbb4afdf6943fff97f737",
-    measurementId: "G-2852GR17V5"
-  };
+  apiKey: 'AIzaSyBR7zQ962PV81jdx6RNYqDuv3SzBrDF_Gs',
+  authDomain: 'waiter-io-f84a1.firebaseapp.com',
+  projectId: 'waiter-io-f84a1',
+  storageBucket: 'waiter-io-f84a1.appspot.com',
+  messagingSenderId: '1035985777599',
+  appId: '1:1035985777599:web:8f167a1535f0bb9985270a',
+  measurementId: 'G-Q0SYLZ8KRG',
+};
---
+++ server/openai.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
+
+router.post('/generate-response', async (req, res) => {
+  try {
+    const apiKey = process.env.OPENAI_API_KEY;
+    const userMessage = req.body.message;
+    console.log(userMessage, 'this is backend');
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/text-davinci-003/completions',
+      {
+        prompt: `say hello world`,
+        max_tokens: 700,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    );
+
+    const generatedResponse = response.data.choices[0].text;
+    res.json({ response: generatedResponse });
+  } catch (error) {
+    console.error('Error generating response:', error);
+    res
+      .status(500)
+      .json({ error: 'An error occurred while generating the response.' });
+  }
+});
+
+module.exports = router;
