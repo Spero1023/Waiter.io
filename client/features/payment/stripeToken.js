b3936b2d0c07ca755c2a7b2b70d81e2ab5c2fc4a
Nicholas <n.spera98@gmail.com>
2023-10-19 17:23:45 -0400

    made page, custom options, need to link for stripe.

--- client/features/payment/stripeToken.js
+++ client/features/payment/stripeToken.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './stripeToken.css';
 
 function Stripe() {
   return (
@@ -7,6 +8,18 @@ function Stripe() {
       <Link to='/'>
         <div className='beta'>home</div>
       </Link>
+      <img className='icon' src='favicon.ico'></img>
+      <div className='logo'>
+        <b>
+          W<span>a</span>iter.<span>io</span>
+        </b>
+      </div>
+      <div className='paymentOptions'>
+        <div className='optionOne'>ADD PAYMENT OPTION 1</div>
+        <div className='optionTwo'>ADD PAYMENT OPTION 2</div>
+        <div className='optionThree'>ADD PAYMENT OPTION 3</div>
+        <div className='optionCustom'>ADD PAYMENT OPTION CUSTOM</div>
+      </div>
     </div>
   );
 }
